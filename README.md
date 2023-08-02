9caf8920513a16a6f56ae35fe0d1c5a36749a0b2
fb0801 <fbhatti0801@gmail.com>
2023-08-02 20:08:37 +0100

    lets get started 3

--- README.md
+++ README.md
@@ -4,4 +4,10 @@ React currency converter
 
 ## Resources used:
 
+https://www.youtube.com/watch?v=XN5elYWiSuw&pp=ygUYcmVhY3QgY3VycmVuY3kgY29udmVydGVy
+
+original repo -> https://github.com/WebDevSimplified/React-Currency-Converter
+
 https://www.toptal.com/developers/gitignore/
+
+https://exchangeratesapi.io/
--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
 import './App.css';
+import CurrencyRow from './CurrencyRow';
+
+const BASE_URL = 'https://api.exchangeratesapi.io/latest'
 
 function App() {
+
+  const [currencyOptions, setCurrencyOptions] = useState([])
+
+  useEffect(() => {
+fetch(BASE_URL)
+.then(res => res.json())
+.then(data => {
+  setCurrencyOptions([])
+})
+  }, [])
+
   return (
+    <>
     <h1>Currency Converter</h1>
-  );
+    <CurrencyRow />
+    <div className='equals'>=</div>
+    <CurrencyRow />
+  </>
+    );
 }
 
 export default App;

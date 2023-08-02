b8028cd34d12665049b71965aae28879b6249357
fb0801 <fbhatti0801@gmail.com>
2023-08-02 17:24:43 +0100

    lets get started 2

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # react-currency-converter
 React currency converter
+
+
+## Resources used:
+
+https://www.toptal.com/developers/gitignore/
--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import App from './App';
+
+
+ReactDOM.createRoot(<App />, document.getElementById('root'));
+
+
 
